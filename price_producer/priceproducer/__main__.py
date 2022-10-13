332a539e00b60c5c1a70171a3847e89e6b38ab80
taegor <kotychka@gmail.com>
2022-10-13 20:09:41 +0300

    Corrected imports

--- price_producer/priceproducer/__main__.py
+++ price_producer/priceproducer/__main__.py
@@ -1,13 +1,15 @@
 import logging
 
-from .price_producer import main as produce_prices
-from .topic_creator import main as create_topics
+from priceproducer.price_producer import main as produce_prices
+from priceproducer.topic_creator import main as create_topics
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+
 def main():
+    """Create topics and start producing prices."""
     create_topics()
-    logger.info('Topics created')
+    logger.info("Topics created")
     produce_prices()
-    logger.info('All topics filled with 100 values')
+    logger.info("All topics filled with 100 values")
